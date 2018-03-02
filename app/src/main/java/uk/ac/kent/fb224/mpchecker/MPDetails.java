5f974f39740f9e357b09ae14ad31a700c0348e0a
FJB <FJBargewell@gmail.com>
2018-03-02 19:44:08 +0000

    fuck all tbh

---
+++ app/src/main/java/uk/ac/kent/fb224/mpchecker/MPDetails.java
@@ -0,0 +1,18 @@
+// Wiki URL: https://en.wikipedia.org/w/api.php?action=query&prop=extracts&exintro&titles=Theresa%20May&format=json
+package uk.ac.kent.fb224.mpchecker;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by Forrest on 02/03/2018.
+ */
+
+public class MPDetails extends AppCompatActivity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+    }
+}
